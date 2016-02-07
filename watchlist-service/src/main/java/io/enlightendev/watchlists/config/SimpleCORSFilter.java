eafe96d7a2a76e5f3bd08fbac085a51f93d7d0a0
EnlightenDev <enlightendevio@gmail.com>
2016-02-07 12:20:34 -0500

    updated CORS config from global config using filter to controller level config. see cookbook

--- watchlist-service/src/main/java/io/enlightendev/watchlists/config/SimpleCORSFilter.java
+++ watchlist-service/src/main/java/io/enlightendev/watchlists/config/SimpleCORSFilter.java
@@ -14,8 +14,8 @@ import java.io.IOException;
  * https://spring.io/guides/gs/rest-service-cors/
  * http://stackoverflow.com/questions/22846309/cors-filter-not-working-as-intended
  */
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
+//@Component
+//@Order(Ordered.HIGHEST_PRECEDENCE)
 public class SimpleCORSFilter implements Filter {
 
 
@@ -23,14 +23,14 @@ public class SimpleCORSFilter implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 
-        log.info("Adding Access Control Response Headers");
-
-        HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, Authorization");
-        chain.doFilter(req, res);
+//        log.info("Adding Access Control Response Headers");
+//
+//        HttpServletResponse response = (HttpServletResponse) res;
+//        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS, DELETE");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, Authorization");
+//        chain.doFilter(req, res);
     }
 
     public void init(FilterConfig filterConfig) {
---
+++ watchlist-service/src/main/java/io/enlightendev/watchlists/web/controller/WatchlistAPIController.java
@@ -0,0 +1,27 @@
+package io.enlightendev.watchlists.web.controller;
+
+import io.enlightendev.watchlists.service.WatchlistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import io.enlightendev.watchlists.model.Watchlist;
+
+/**
+ *
+ */
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@RestController
+@RequestMapping("/api")
+public class WatchlistAPIController {
+
+    @Autowired
+    private WatchlistService watchlistService;
+
+    @RequestMapping("/watchlist")
+    public List<Watchlist> getWatchLists(){
+        return watchlistService.getWatchLists();
+    }
+}
